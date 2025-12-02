e7f1609220941bf9845c563a245c3e272258c918
Edison <edison.puerta714@gmail.com>
2025-12-02 21:50:41 +0100

    Backend Sprint 2: Produkt-, Detail-, Kategorien- und Warenkorb-Logik implementiert und getestet

--- public/produktdetails.php
+++ public/produktdetails.php
@@ -1,1 +1,48 @@
-<?php include '../includes/navigation.php'; ?>
+<?php
+require_once '../includes/header.php';
+require_once '../includes/navigation.php';
+require_once '../includes/db.php';
+require_once '../includes/functions.php';
+
+
+if (!isset($_GET['id'])) {
+    die("Produkt nicht gefunden.");
+}
+
+$produkt_id = intval($_GET['id']);
+$produkt = getProductById($conn, $produkt_id);
+
+if (!$produkt) {
+    die("Produkt existiert nicht.");
+}
+?>
+
+<h2><?php echo htmlspecialchars($produkt['produktname']); ?></h2>
+
+<div class="produkt-details">
+    <img class="produkt-details-bild"
+        src="../<?php htmlspecialchars($produkt['hauptbild']); ?>" 
+        alt="<?php echo htmlspecialchars($produkt['produktname']); ?>">
+
+    <div class="produkt-info">
+
+    <h3><?php echo number_format($produkt['preis'], 2, ',', '.') ?> €</h3>
+
+    <p><?php echo nl2br(htmlspecialchars($produkt['produktbeschreibung'])); ?></p>
+
+    <h4>Farben:</h4>
+    <ul>
+        <?php 
+        $farben = getColorsByProduct($conn, $produkt_id); 
+        while ($f = $farben->fetch_assoc()): 
+        ?>
+        <li><?php echo htmlspecialchars($f['farbe']); ?></li>
+        <?php endwhile; ?>
+    </ul>
+    </div>
+</div>
+
+<?php require_once '../includes/footer.php'; ?>
+
+<!-- Correct CSS Path -->
+<link rel="stylesheet" href="../assets/css/styles.css">
--- public/produkte.php
+++ public/produkte.php
@@ -6,24 +6,28 @@ require_once '../includes/functions.php';
 ?>
 
 <h2>Produkte</h2>
+<!-- Correct CSS Path -->
+<link rel="stylesheet" href="../assets/css/styles.css">
 
 <div class="produkt-grid">
 
 <?php
 $produkte = getAllProducts($conn);
 
-while ($p = $produkte->fetch_assoc())
+while ($p = $produkte->fetch_assoc()):
 ?>
 
     <div class="produkt-card">
         <img src="../<?php echo htmlspecialchars($p['hauptbild']);?>" alt="<?php echo htmlspecialchars($p['produktname']);?>" class="produkt-bild">
 
-        <h3><?php echo htmlspecialchars($p['produkname']);?></h3>
+        <h3><?php echo htmlspecialchars($p['produktname']);?></h3>
         <p><?php echo number_format($p['preis'],2, ',', '.'); ?> €</p>
         <a href="produktdetails.php?id=<?php echo $p['produkt_id'];?>" class="btn">Detais ansehen</a>
     </div>
 
-endwhile; ?>    
+<?php
+endwhile; 
+?>    
 </div>
 
 <?php require_once '../includes/footer.php'; ?>
