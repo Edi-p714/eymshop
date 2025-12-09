488193a9542adba2885caa4ef2f93acdc082a720
Edison <edison.puerta714@gmail.com>
2025-12-09 19:15:28 +0100

    Style: Footer-Redesign (Grid-Layout) & Layout-Fix auf Über-uns-Seite

--- includes/footer.php
+++ includes/footer.php
@@ -3,30 +3,33 @@
     <div class="footer-content">
         <div class="footer-section">
             <h3>Kontakt</h3>
+            <ul class="footer-links">
+                <li><a href="#">📞 0176767676 </a></li> <!-- Emoji telefon -->
+                <li><a href="mailto:Eymshop@gmail.com"> 📧 Eymshop@gmail.com </a></li> <!-- Emoji Gmail -->
+                <li><a href="#">📍 Hannover 30455 </a></li> <!-- Emoji name ferestadan -->
+            </ul>
         </div>
 
+        
         <div class="footer-section">
-            <ul>
-                <li><a href="#">0176767676 📞</a></li> <!-- Emoji telefon -->
-                <li><a href="#">Eymshop@gmail.com 📧</a></li> <!-- Emoji Gmail -->
-                <li><a href="#">Hannover 30455 📍</a></li> <!-- Emoji name ferestadan -->
+            <h3>Folgen Sie uns:</h3>
+            <ul class="footer-links">
+                <li><a href="#">📸 Instagram</a></li> <!-- Emoji Gmail -->
+                <li><a href="#">🐦 twitter. / X</a></li>
             </ul>
         </div>
 
         <div class="footer-section">
-            <h3>Folgen Sie uns:</h3>
-        </div>
-
-        <div class="footer-section">
-            <ul>
-                <li><a href="#">Instagram.com/Eymshop </a></li> <!-- Emoji Gmail -->
-                <li><a href="#">twitter.com/Eymshop</a></li>
+            <h3>Rechtliches</h3>
+            <ul class="footer-links">
+                <li><a href="datenschutz.php">🔒 Datenschutz</a></li>
+                <li><a href="">⚖️ Impressum</a></li>
             </ul>
         </div>
     </div>
 
     <div class="footer-bottom">
-        <p><a href="datenschutz.php" class="footer-link">Datenschutz</a></p>
         <p>Alle Rechte vorbehalten © 2025 Eymshop</p>
     </div>
+
 </footer>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,13 +1,10 @@
 <nav class="main-nav">
     <ul class="nav-menu">
         <li><a href="index.php">Startseite</a></li>
-
         <li><a href="produkte.php">Produkte</a></li>
-
         <li><a href="produkte.php?kategorie=Herren">Herren</a></li> 
-
         <li><a href="produkte.php?kategorie=Damen">Damen</a></li>
-
+        <li><a href="produkte.php?kategorie=Mädchen">Mädchen</a></li>
         <li><a href="uberuns.php">Über uns</a></li>
         <li><a href="kontakt.php">Kontakt</a></li>
     </ul>
